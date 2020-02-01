19a8b65fc25e780f3a7302ccaf913f6922561cb5
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-01-31 23:29:17 -0700

    checked for Android Studio use

--- RideOn/ride_on/lib/main.dart
+++ RideOn/ride_on/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'RideOn Home Page'),
     );
   }
 }
