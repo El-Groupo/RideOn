5ec56fcce8803a468b25a118add079c478fd1c8a
Nbbyers15 <57930194+Nbbyers15@users.noreply.github.com>
2020-02-21 19:26:03 -0700

    connect Firebase iOS app
    I added the google services.plist and some information in the AppDelegate.swift

--- ride_on/ride_on/ios/Runner/AppDelegate.swift
+++ ride_on/ride_on/ios/Runner/AppDelegate.swift
@@ -1,13 +1,25 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UIResponder, UIApplicationDelegate 
+{
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  ) -> Bool 
+  {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  var window: UIWindow?
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool 
+      {
+        FirebaseApp.configure()
+        return true
+      }
 }
