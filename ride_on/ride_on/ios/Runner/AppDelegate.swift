bc8a8c953476cfd7702f1d76b2c4429df0f96605
Walker Thornley <55719041+thornleywalker@users.noreply.github.com>
2020-04-25 19:51:42 -0700

    stuff

--- ride_on/ride_on/ios/Runner/AppDelegate.swift
+++ ride_on/ride_on/ios/Runner/AppDelegate.swift
@@ -7,19 +7,23 @@ import Firebase
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate/*, UIResponder, UIApplicationDelegate */
 {
+    
+    var newwindow: UIWindow?
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool 
   {
     FirebaseApp.configure() //inserted from below
+
     GeneratedPluginRegistrant.register(with: self)
     GMSServices.provideAPIKey("AIzaSyA1yYLr9YgOewrJrfdrvXfQB9faw97GBGM")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     
   }
 
- //var window: UIWindow?
+    
     /*
     override func application(_ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions:
